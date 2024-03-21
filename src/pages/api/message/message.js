737ec436b5810284ec843a4cfd2ccf6e4af6f593
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2024-03-21 11:08:29 +0530

    LL

--- src/pages/api/message/message.js
+++ src/pages/api/message/message.js
@@ -20,7 +20,8 @@ export default async function handler(req, res) {
         const contact = req.body.entry[0].changes[0].value.contacts[0];
         const contactName = contact.profile.name;
         const contactNumber = contact.wa_id;
-        await sendMessage(contactNumber, `Hello ${contactName} from your RestoAI!`);
+        const accessToken="EAAIqSsKeP0QBOx6KtD1xzZBAghkvVvbsZC297LcmEWI22ffUK3mRUPaP5ItCkQZBmBr4KvCyM4p8Bdyd20Me91tBVhcWDJgyjCWR0qXdXmkCLFKol8KxuvIZAIS0R3ZBUtwJjZBue7HzU0NHm5z6AYxsTG3GKhLSRx83q5uwLdHofkO0ZCo7qw031YfUZCPwQRTeSAXTG81URLoBo3HE7RsZD";
+        await sendMessage(contactNumber, `Hello ${contactName} from your RestoAI!`, accessToken);
 
         res.status(200).end();
     } else {
@@ -29,17 +30,25 @@ export default async function handler(req, res) {
 }
 
 // Function to send message using Facebook Messenger API
-async function sendMessage(recipientPhoneNumber, message) {
+async function sendMessage(recipientPhoneNumber, message, accessToken) {
     try {
-        const response = await axios.post('https://graph.facebook.com/v18.0/me/messages?access_token=EAAIqSsKeP0QBOx6KtD1xzZBAghkvVvbsZC297LcmEWI22ffUK3mRUPaP5ItCkQZBmBr4KvCyM4p8Bdyd20Me91tBVhcWDJgyjCWR0qXdXmkCLFKol8KxuvIZAIS0R3ZBUtwJjZBue7HzU0NHm5z6AYxsTG3GKhLSRx83q5uwLdHofkO0ZCo7qw031YfUZCPwQRTeSAXTG81URLoBo3HE7RsZD', {
+        const config = {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const response = await axios.post('https://graph.facebook.com/v18.0/me/messages', {
             messaging_type: 'MESSAGE_TAG',
             recipient: {
                 phone_number: recipientPhoneNumber
             },
             message: {
                 text: message
+            
             }
-        });
+        }, config);
 
         console.log("Message sent:", response.data);
     } catch (error) {
