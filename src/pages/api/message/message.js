30bcf6a8cac0d99b5d094ce62d9546fdc69ea2b0
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2024-03-22 10:51:18 +0530

    ll

--- src/pages/api/message/message.js
+++ src/pages/api/message/message.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import client from '../../../models/client';
+import connectDb from '../../middleware/connectDb';
 import Cryptr from 'cryptr';
+
 const cryptr = new Cryptr('mysecret');
-export default async function handler(req, res) {
+
+const handler = async (req, res) => {
     if (req.method === 'GET') {
         if (
             req.query["hub.mode"] === "subscribe" &&
@@ -18,23 +21,31 @@ export default async function handler(req, res) {
         // Extract message body from the request
         const messageBody = req.body.entry[0].changes[0].value.messages[0].text.body;
         console.log("Received message: " + messageBody);
+
         // Access table property after decryption
         const table = JSON.parse(messageBody).table;
+        
         const contact = req.body.entry[0].changes[0].value.contacts[0];
         const contactName = contact.profile.name;
         const contactNumber = contact.wa_id;
-        const cli = await client.findOne({ phone: contactNumber });
-        if(!cli){
-            cli.push({name:contactName,phone:contactNumber});
-            await cli.save()
+        
+        let cli = await client.findOne({ phone: contactNumber });
+        if (!cli) {
+            // If client not found, create a new document
+            cli = new client({ name: contactName, phone: contactNumber });
+            await cli.save();
         }
-        const data= cryptr.encrypt({
-            name : contactName ,
-            number : contactNumber , 
-            msg : messageBody 
-        })
+        
+        const data = cryptr.encrypt({
+            name: contactName,
+            number: contactNumber,
+            msg: messageBody
+        });
+
         const accessToken = "EAAIqSsKeP0QBOx6KtD1xzZBAghkvVvbsZC297LcmEWI22ffUK3mRUPaP5ItCkQZBmBr4KvCyM4p8Bdyd20Me91tBVhcWDJgyjCWR0qXdXmkCLFKol8KxuvIZAIS0R3ZBUtwJjZBue7HzU0NHm5z6AYxsTG3GKhLSRx83q5uwLdHofkO0ZCo7qw031YfUZCPwQRTeSAXTG81URLoBo3HE7RsZD";
+
         await sendMessage(contactNumber, `Hello ${contactName} from your RestoAI! \n click the link to order \n https://posystem.onrender.com/menu?data=${data}`, accessToken);
+        
         res.status(200).end();
     } else {
         res.status(405).json({ message: 'Method Not Allowed' });
@@ -67,3 +78,5 @@ async function sendMessage(recipientPhoneNumber, message, accessToken) {
         console.error("Error sending message:", error);
     }
 }
+
+export default connectDb(handler);
