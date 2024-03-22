11cbe079eb1c3b51b0c57528b366826487ae4fe9
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2024-03-22 10:26:00 +0530

    ll

--- src/pages/api/message/message.js
+++ src/pages/api/message/message.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import client from '@/models/client';
+import client from '../../../models/client';
 import Cryptr from 'cryptr';
-const cryptr=new Cryptr('mysecret');
+const cryptr = new Cryptr('mysecret');
 export default async function handler(req, res) {
     if (req.method === 'GET') {
         if (
@@ -18,8 +18,6 @@ export default async function handler(req, res) {
         // Extract message body from the request
         const messageBody = req.body.entry[0].changes[0].value.messages[0].text.body;
         console.log("Received message: " + messageBody);
-        const decryptedMessageBody = cryptr.decrypt(messageBody);
-
         // Access table property after decryption
         const table = JSON.parse(decryptedMessageBody).table;
         const contact = req.body.entry[0].changes[0].value.contacts[0];
@@ -30,8 +28,13 @@ export default async function handler(req, res) {
             cli.push({name:contactName,phone:contactNumber});
             await cli.save()
         }
+        const data= cryptr.encrypt({
+            name : contactName ,
+            number : contactNumber , 
+            msg : messageBody 
+        })
         const accessToken = "EAAIqSsKeP0QBOx6KtD1xzZBAghkvVvbsZC297LcmEWI22ffUK3mRUPaP5ItCkQZBmBr4KvCyM4p8Bdyd20Me91tBVhcWDJgyjCWR0qXdXmkCLFKol8KxuvIZAIS0R3ZBUtwJjZBue7HzU0NHm5z6AYxsTG3GKhLSRx83q5uwLdHofkO0ZCo7qw031YfUZCPwQRTeSAXTG81URLoBo3HE7RsZD";
-        await sendMessage(contactNumber, `Hello ${contactName} from your RestoAI! \n click the link to order \n https://posystem.onrender.com/menu?data=${decryptedMessageBody}`, accessToken);
+        await sendMessage(contactNumber, `Hello ${contactName} from your RestoAI! \n click the link to order \n https://posystem.onrender.com/menu?data=${data}`, accessToken);
         res.status(200).end();
     } else {
         res.status(405).json({ message: 'Method Not Allowed' });
