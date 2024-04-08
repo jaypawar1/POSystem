adb6a22c655d1cad7563f673df02bf09a55a78b0
Sachin Parihar <pariharsachin5002@gmail.com>
2024-04-08 19:20:33 +0530

    template section

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    
+     <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+    rel="stylesheet"
+  />
       <body className={inter.className}>{children}</body>
     </html>
   );
