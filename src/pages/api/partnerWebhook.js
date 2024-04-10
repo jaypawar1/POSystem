8c82cc8e151e11168e738e89ddbfe5464e721fda
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2024-04-10 12:16:10 +0530

    partner webhook

---
+++ src/pages/api/partnerWebhook.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import client from '../../../models/client';
+import connectDb from '../../../middleware/connectDb';
+import Cryptr from 'cryptr';
+
+const cryptr = new Cryptr('mysecret');
+
+const handler = async (req, res) => {
+    if (req.method === 'POST') {
+        const sharedSecret = '5c96789d5ec4e8ec8f834'; // Replace with your actual shared secret
+        const receivedSignature = req.headers['x-aisensy-signature'];
+        const requestBody = JSON.stringify(req.body);
+
+        // Calculate signature from the request body using the shared secret
+        const hmac = crypto.createHmac('sha256', sharedSecret);
+        const calculatedSignature = hmac.update(requestBody).digest('hex');
+
+        // Compare received signature with calculated signature
+        if (receivedSignature === calculatedSignature) {
+            // Signature is valid, process the webhook events
+            console.log('Received webhook events:', req.body);
+            res.status(200).send('Webhook events received and validated.',req.body);
+        } else {
+            // Signature is invalid, reject the request
+            console.error('Invalid signature. Webhook request rejected.');
+            res.status(403).send('Invalid signature.');
+        }
+    } else {
+        res.status(405).json({ message: 'Method Not Allowed' });
+    }
+}
+
+export default connectDb(handler);
