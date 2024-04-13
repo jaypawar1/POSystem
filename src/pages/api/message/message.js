058907014a52c96338cc9d3b86e23cfc7abadf52
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2024-04-13 16:43:59 +0530

    L

--- src/pages/api/message/message.js
+++ src/pages/api/message/message.js
@@ -4,6 +4,16 @@ export default function handler(req, res) {
   if (req.method === 'POST') {
     try {
       console.log(JSON.stringify(req.body));
+      const entry = res.body.entry[0];
+      const senderName = entry.changes[0].value.contacts[0].profile.name;
+      const senderNumber = entry.changes[0].value.contacts[0].wa_id;
+      const message = entry.changes[0].value.messages[0].text.body;
+      
+      // Now you can use these variables as needed
+      console.log("Sender Name:", senderName);
+      console.log("Sender Number:", senderNumber);
+      console.log("Message:", message);
+
       res.status(500).send(JSON.stringify(req.body))
     } catch(err) {
       console.error(err);
