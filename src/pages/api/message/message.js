1b1a75790a87edfb8efbcd4d76036dd9cfd7b1e8
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2024-04-13 17:00:18 +0530

    L

--- src/pages/api/message/message.js
+++ src/pages/api/message/message.js
@@ -1,21 +1,68 @@
-import { createHmac } from 'crypto';
-
+import axios from 'axios';
 export default function handler(req, res) {
   if (req.method === 'POST') {
     try {
+      const authorizationHeader = req.headers['authorization'];
       console.log(JSON.stringify(req.body));
       const entry = res.body.entry[0];
       const senderName = entry.changes[0].value.contacts[0].profile.name;
       const senderNumber = entry.changes[0].value.contacts[0].wa_id;
       const message = entry.changes[0].value.messages[0].text.body;
-      
-      // Now you can use these variables as needed
       console.log("Sender Name:", senderName);
       console.log("Sender Number:", senderNumber);
       console.log("Message:", message);
+      const requestData = {
+        to: senderNumber,
+        type: "template",
+        template: {
+          language: {
+            policy: "deterministic",
+            code: "en"
+          },
+          name: "menulink",
+          components: [
+            {
+              type: "body",
+              parameters: [
+                {
+                  type: "text",
+                  text: senderName
+                },
+                {
+                  type: "text",
+                  text: senderName
+                },
+                {
+                  type: "text",
+                  text: senderName
+                },
+                {
+                  type: "text",
+                  text: senderNumber
+                }
+              ]
+            }
+          ]
+        }
+      };
 
+      // Define the headers
+      const headers = {
+        'Accept': 'application/json, application/xml',
+        'Authorization': `${authorizationHeader}`,
+        'Content-Type': 'application/json'
+      };
+
+      // Make the Axios POST request
+      axios.post('https://backend.aisensy.com/direct-apis/t1/messages', requestData, { headers })
+        .then(response => {
+          console.log('Response:', response.data);
+        })
+        .catch(error => {
+          console.error('Error:', error.response.data);
+        });
       res.status(500).send(JSON.stringify(req.body))
-    } catch(err) {
+    } catch (err) {
       console.error(err);
       res.status(500).send("An error occurred while verifying the signature");
     }
